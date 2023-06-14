5eb2ad415eb7d18ef16f218429e7e08f41b3b4c4
Sgatollo <rahmanhabeebur878@gmail.com>
2023-06-14 16:22:55 +0200

    Creato TabBar, fixato stili e la BottomBar Navigation
    Ho fatto una floating TabBar, sistemato tutti gli stili di essa, ho cercato di fare in modo che il pulsante centrale sbordasse un po' ma veniva tagliato ;(, quindi non l'ho fatto. Inoltre ho migliorato la navigazione

--- App.js
+++ App.js
@@ -1,46 +1,48 @@
 import {
-   StyleSheet,
-   Text,
-   SafeAreaView,
-   TouchableWithoutFeedback,
-   Platform,
-   StatusBar,
-   View,
-} from 'react-native';
-import Colors from './src/components/Colors';
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  Platform,
+  StatusBar,
+  View,
+} from "react-native";
+import Colors from "./src/components/colors";
 
-import Home from './src/screens/Home';
-import Search from './src/screens/Search';
-import Scan from './src/screens/Scan';
-import Login from './src/screens/Login';
-import TabBar from './src/components/TabBar';
+import { useEffect, useState } from "react";
 
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Home from "./src/screens/Home";
+import Search from "./src/screens/Search";
+import Scan from "./src/screens/Scan";
+import Login from "./src/screens/Login";
+import TabBar from "./src/components/TabBar";
+
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-   return (
-      <>
-         <StatusBar animated={true} barStyle={'default'} />
-         <TabBar />
-      </>
-   );
+  return (
+    <>
+      <StatusBar animated={true} barStyle={"default"} />
+      <TabBar />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: Colors.green,
-      justifyContent: 'space-between', // or 'center'
-      alignItems: 'flex-start', // or 'center'
-      padding: 30,
-      paddingTop: Platform.OS === 'android' ? 60 : 0,
-      flexDirection: 'row',
-   },
-   testo: {
-      fontSize: 24,
-   },
+  container: {
+    flex: 1,
+    backgroundColor: Colors.green,
+    justifyContent: "space-between", // or 'center'
+    alignItems: "flex-start", // or 'center'
+    padding: 30,
+    paddingTop: Platform.OS === "android" ? 60 : 0,
+    flexDirection: "row",
+  },
+  testo: {
+    fontSize: 24,
+  },
 });
