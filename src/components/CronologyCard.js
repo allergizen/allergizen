94492738903efb1373dce4bfe50e0457d8e4a2d9
Rillosi Stefano <rillosi.stefano.studente@itispaleocapa.it>
2023-06-16 10:40:32 +0200

    Update CronologyCard.js
    add useState

--- src/components/CronologyCard.js
+++ src/components/CronologyCard.js
@@ -4,47 +4,42 @@ import React from 'react';
 import Colors from './Colors';
 
 function CronologyCard({ item }) {
-  return (
-    <View style={styles.cronologyCardStyle}>
-      <View style={{ flex: 1, padding: 15 }}>
-        <Image
-          source={{ uri: item.img }}
-          style={styles.imageBackground}
-          resizeMode='contain'
-        />
+   const [image, setImage] = React.useState('');
+   return (
+      <View style={styles.cronologyCardStyle}>
+         <View style={{ flex: 1, padding: 15 }}>
+            <Image source={{ uri: item.img }} style={styles.imageBackground} resizeMode='contain' />
+         </View>
+         <View style={{ flex: 1, alignSelf: 'center' }}>
+            <Text style={{ flex: 5, alignSelf: 'center', fontSize: 16 }}>{item.name}</Text>
+         </View>
       </View>
-      <View style={{ flex: 1, alignSelf: 'center' }}>
-        <Text style={{ flex: 5, alignSelf: 'center', fontSize: 16 }}>
-          {item.name}
-        </Text>
-      </View>
-    </View>
-  );
+   );
 }
 
 const styles = StyleSheet.create({
-  cronologyCardStyle: {
-    width: '90%',
-    height: 200,
-    flex: 1,
-    flexDirection: 'row',
-    margin: 5,
-    borderRadius: 25,
-    padding: 5,
-    backgroundColor: '#fff',
-    alignItems: 'space-between',
-    justifyContent: 'center',
-    alignSelf: 'center',
-  },
-  imageView: {
-    borderRadius: 8,
-    flex: 2,
-  },
-  imageBackground: {
-    flex: 1,
-    minWidth: 100,
-    overflow: 'hidden',
-  },
+   cronologyCardStyle: {
+      width: '90%',
+      height: 200,
+      flex: 1,
+      flexDirection: 'row',
+      margin: 5,
+      borderRadius: 25,
+      padding: 5,
+      backgroundColor: '#fff',
+      alignItems: 'space-between',
+      justifyContent: 'center',
+      alignSelf: 'center',
+   },
+   imageView: {
+      borderRadius: 8,
+      flex: 2,
+   },
+   imageBackground: {
+      flex: 1,
+      minWidth: 100,
+      overflow: 'hidden',
+   },
 });
 
 export default CronologyCard;
