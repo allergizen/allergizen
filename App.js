199f43121d97edf4bc1ccad322b4580183c3ccfc
Rillosi Stefano <rillosi.stefano.studente@itispaleocapa.it>
2023-06-16 10:38:26 +0200

    FIx duplicate firebase initialization

--- App.js
+++ App.js
@@ -11,39 +11,36 @@ import TabBar from './src/components/TabBar';
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [userToken, setUserToken] = React.useState('asd');
+   const [userToken, setUserToken] = React.useState('asd');
 
-  // React.useEffect(() => {
-  //   // Verifica se l'utente è già autenticato e imposta il token
-  //   // const checkUserToken = () => {
-  //   //   const token = setUserToken(token); // Recupera il token di autenticazione dall'archiviazione o da altro luogo;
-  //   // };
+   // React.useEffect(() => {
+   //   // Verifica se l'utente è già autenticato e imposta il token
+   //   // const checkUserToken = () => {
+   //   //   const token = setUserToken(token); // Recupera il token di autenticazione dall'archiviazione o da altro luogo;
+   //   // };
 
-  //   // checkUserToken();
-  // }, []);
+   //   // checkUserToken();
+   // }, []);
 
-  return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={
-            userToken ? (userToken.length > 0 ? 'Home' : 'Login') : 'Login'
-          }
-          headerMode='none'
-        >
-          <Stack.Screen name='Login' component={Login} />
-          <Stack.Screen name='Signup' component={Signup} />
-          <Stack.Screen name='Home'>{() => <TabBar />}</Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style='auto' />
-    </>
-  );
+   return (
+      <>
+         <NavigationContainer>
+            <Stack.Navigator
+               initialRouteName={userToken ? (userToken.length > 0 ? 'Home' : 'Login') : 'Login'}
+               headerMode='none'>
+               <Stack.Screen name='Login' component={Login} />
+               <Stack.Screen name='Signup' component={Signup} />
+               <Stack.Screen name='Home'>{() => <TabBar />}</Stack.Screen>
+            </Stack.Navigator>
+         </NavigationContainer>
+         <StatusBar style='auto' />
+      </>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+   },
 });
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,191 +1,180 @@
-import React from "react";
-import {
-  KeyboardAvoidingView,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from "react-native";
+import React from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, setDoc, doc } from "firebase/firestore/lite";
-import { KEY, AD, PRID, STBU, MSI, AI } from "@env";
-import TextInput from "react-native-textinput-with-icons";
-import Globals from "../assets/Globals.js";
-
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, setDoc, doc } from 'firebase/firestore/lite';
+import { KEY, AD, PRID, STBU, MSI, AI } from '@env';
+import TextInput from 'react-native-textinput-with-icons';
+import Globals from '../assets/Globals.js';
+import { app } from '../screens/Login';
 const firebaseConfig = {
-  apiKey: KEY,
-  authDomain: AD,
-  projectId: PRID,
-  storageBucket: STBU,
-  messagingSenderId: MSI,
-  appId: AI,
+   apiKey: KEY,
+   authDomain: AD,
+   projectId: PRID,
+   storageBucket: STBU,
+   messagingSenderId: MSI,
+   appId: AI,
 };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function adddb(email,name,user){
-   try{
-      await setDoc(doc(db, "users", user), {
+async function adddb(email, name, user) {
+   try {
+      await setDoc(doc(db, 'users', user), {
          nome: name,
          email: email,
          allergie: {
-            glutine:false, 
-            crostacei:false, 
-            uova:false, 
-            pesce:false,
-            arachidi:false, 
-            soia:false, 
-            latte:false, 
-            fruttaaguscio:false, 
-            sedano:false, 
-            senape:false, 
-            sesamo:false, 
-            anidridesolforosa:false, 
-            lupini:false, 
-            molluschi:false
+            glutine: false,
+            crostacei: false,
+            uova: false,
+            pesce: false,
+            arachidi: false,
+            soia: false,
+            latte: false,
+            fruttaaguscio: false,
+            sedano: false,
+            senape: false,
+            sesamo: false,
+            anidridesolforosa: false,
+            lupini: false,
+            molluschi: false,
          },
       });
-  }catch{
-      console.log("error");
-  }
+   } catch {
+      console.log('error');
+   }
 }
 
 const Signup = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [confirmpassword, setConfirmPassword] = React.useState("");
-  const [name,setName] = React.useState("");
+   const [email, setEmail] = React.useState('');
+   const [password, setPassword] = React.useState('');
+   const [confirmpassword, setConfirmPassword] = React.useState('');
+   const [name, setName] = React.useState('');
 
-  const handleSignup = () => {
-   if(password != confirmpassword){
-      alert("Le password non coincidono");
-      return;
-   }
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        console.log("signed up: ", user.email);
-         adddb(email,name,user.uid);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
-  return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <View style={styles.signupView}>
-        <Text style={styles.title}>Signup</Text>
-      </View>
+   const handleSignup = () => {
+      if (password != confirmpassword) {
+         alert('Le password non coincidono');
+         return;
+      }
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, email, password)
+         .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            console.log('signed up: ', user.email);
+            adddb(email, name, user.uid);
+         })
+         .catch((error) => {
+            alert(error.message);
+         });
+   };
+   return (
+      <KeyboardAvoidingView style={styles.container} behavior='padding'>
+         <View style={styles.signupView}>
+            <Text style={styles.title}>Signup</Text>
+         </View>
 
-      <View style={styles.inputContainer}>
-      <TextInput
-          leftIcon="person-circle"
-          style={styles.input}
-          value={name}
-          onChangeText={(text) => setName(text)}
-        />
-      <TextInput
-          leftIcon="mail"
-          style={styles.input}
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          leftIcon="lock-closed"
-          style={styles.input}
-          secureTextEntry
-          value={password}
-          onChangeText={(password) => setPassword(password)}
-        />
-        <TextInput
-          leftIcon="lock-closed"
-          style={styles.input}
-          secureTextEntry
-          value={confirmpassword}
-          onChangeText={(confirmpassword) => setConfirmPassword(confirmpassword)}
-        />
-      </View>
+         <View style={styles.inputContainer}>
+            <TextInput
+               leftIcon='person-circle'
+               style={styles.input}
+               value={name}
+               onChangeText={(text) => setName(text)}
+            />
+            <TextInput
+               leftIcon='mail'
+               style={styles.input}
+               value={email}
+               onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+               leftIcon='lock-closed'
+               style={styles.input}
+               secureTextEntry
+               value={password}
+               onChangeText={(password) => setPassword(password)}
+            />
+            <TextInput
+               leftIcon='lock-closed'
+               style={styles.input}
+               secureTextEntry
+               value={confirmpassword}
+               onChangeText={(confirmpassword) => setConfirmPassword(confirmpassword)}
+            />
+         </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleSignup} style={styles.button}>
-          <Text style={styles.buttonText}>Sign up</Text>
-        </TouchableOpacity>
+         <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={handleSignup} style={styles.button}>
+               <Text style={styles.buttonText}>Sign up</Text>
+            </TouchableOpacity>
 
-        <View style={styles.register}>
-          <Text style={styles.text}>Not new? </Text>
-          <TouchableOpacity onPress={() => {}} style={styles.textAsBtn}>
-            <Text style={styles.textAsBtn}>Login</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </KeyboardAvoidingView>
-  );
+            <View style={styles.register}>
+               <Text style={styles.text}>Not new? </Text>
+               <TouchableOpacity onPress={() => {}} style={styles.textAsBtn}>
+                  <Text style={styles.textAsBtn}>Login</Text>
+               </TouchableOpacity>
+            </View>
+         </View>
+      </KeyboardAvoidingView>
+   );
 };
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
- 
-    paddingHorizontal: Globals.css.HorizontalPaddingView,
-  },
-  input: {
-    backgroundColor: "#fff",
-    paddingVertical: 17,
-    borderRadius: 10,
-    marginTop: 10,
-  },
-  buttonContainer: {
-    justifyContent: "center",
-    marginTop: 40,
-  },
-  button: {
-    backgroundColor: "#F9DF9F",
-    marginTop: 5,
-    paddingHorizontal: 90,
-    paddingVertical: 15,
-    borderRadius: 15,
-  },
-  textAsBtn: {
-    color: "#DAAF53",
-    fontWeight: "bold",
-    textAlign: "right",
-  },
+   container: {
+      flex: 1,
+      justifyContent: 'center',
 
-  forgotContainer: {
-    marginTop: 10,
-    
-  },
-  text: {
-    fontWeight: "normal",
-  },
-  buttonText: {
-    fontWeight: "semibold",
-    fontSize: 20,
-    textAlign: "center",
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 40,
-    justifyContent: "start",
-    
-  },
-  register: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  inputContainer: {
-    marginLeft: 15,
+      paddingHorizontal: Globals.css.HorizontalPaddingView,
+   },
+   input: {
+      backgroundColor: '#fff',
+      paddingVertical: 17,
+      borderRadius: 10,
+      marginTop: 10,
+   },
+   buttonContainer: {
+      justifyContent: 'center',
+      marginTop: 40,
+   },
+   button: {
+      backgroundColor: '#F9DF9F',
+      marginTop: 5,
+      paddingHorizontal: 90,
+      paddingVertical: 15,
+      borderRadius: 15,
+   },
+   textAsBtn: {
+      color: '#DAAF53',
+      fontWeight: 'bold',
+      textAlign: 'right',
+   },
 
-  },
+   forgotContainer: {
+      marginTop: 10,
+   },
+   text: {
+      fontWeight: 'normal',
+   },
+   buttonText: {
+      fontWeight: 'semibold',
+      fontSize: 20,
+      textAlign: 'center',
+   },
+   title: {
+      fontWeight: 'bold',
+      fontSize: 40,
+      justifyContent: 'start',
+   },
+   register: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginTop: 20,
+   },
+   inputContainer: {
+      marginLeft: 15,
+   },
 });
-
 
 export default Signup;
