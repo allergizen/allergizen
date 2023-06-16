f9e871c641abad6704e3bb9a496261767be9e34b
Sgatollo <rahmanhabeebur878@gmail.com>
2023-06-16 09:42:49 +0200

    Update Search.js

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -2,17 +2,22 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React from 'react';
 
 const Search = () => {
-   return (
-      <>
-         <TextInput
-            style={{ height: 45, width: '95%', borderColor: 'gray', borderWidth: 2 }}
-            placeholder="Search"
-            underlineColorAndroid="transparent"
-         />
-      </>
-   );
+  return (
+    <>
+      <TextInput
+        style={{
+          height: 45,
+          width: '95%',
+          borderColor: 'gray',
+          borderWidth: 2,
+        }}
+        placeholder='Search'
+        underlineColorAndroid='transparent'
+      />
+    </>
+  );
 };
 
-export default Search;
-
 const styles = StyleSheet.create({});
+
+export default Search;
