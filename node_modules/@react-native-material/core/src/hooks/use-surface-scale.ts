0bb388b523109aa99aff9e73b4964a924d44fcc0
Nicola Preda <nicola.preda05@gmail.com>
2023-08-29 11:21:21 +0200

    Delete node_modules directory

--- node_modules/@react-native-material/core/src/hooks/use-surface-scale.ts
+++
@@ -1,9 +0,0 @@
-import { useTheme } from '../base/ThemeContext';
-import { useMemo } from 'react';
-import chroma from 'chroma-js';
-
-export const useSurfaceScale = () => {
-  const { palette } = useTheme();
-
-  return useMemo(() => chroma.scale([palette.surface.main, palette.surface.on]), [palette.surface]);
-};
--- node_modules/react-native-config/ios/ReactNativeConfig/RNCConfig.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface RNCConfig : NSObject
-
-+ (NSDictionary *)env;
-+ (NSString *)envFor: (NSString *)key;
-
-@end
